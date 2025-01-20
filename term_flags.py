783d37be311d81dc2fb0abc96a8a774fd3f68e4d
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-01-20 17:43:33 +0100

    sub-character lengths

--- term_flags.py
+++ term_flags.py
@@ -54,29 +54,54 @@ LS = LineSegment
 # See: https://www.hrc.org/resources/lgbtq-pride-flags
 # See: https://www.volvogroup.com/en/news-and-media/news/2021/jun/lgbtq-pride-flags-and-what-they-stand-for.html
 ProgressPrideFlag: Flag = [
-    [LS(TransPink, 1, TD), LS(TransBlue, 2, TD), LS(Brown, 2, TD), LS(Black, 2, TD), LS(Red, 13, SQ)],
-    [LS(White, 1, TD), LS(TransPink, 2, TD), LS(TransBlue, 2, TD), LS(Brown, 2, TD), LS(Black, 2, TD), LS(Orange, 11, SQ)],
-    [LS(White, 2, TD), LS(TransPink, 2, TD), LS(TransBlue, 2, TD), LS(Brown, 2, TD), LS(Black, 2, TD), LS(Yellow, 10, SQ)],
-    [LS(White, 2, TU), LS(TransPink, 2, TU), LS(TransBlue, 2, TU), LS(Brown, 2, TU), LS(Black, 2, TU), LS(Green, 10, SQ)],
-    [LS(White, 1, TU), LS(TransPink, 2, TU), LS(TransBlue, 2, TU), LS(Brown, 2, TU), LS(Black, 2, TU), LS(Blue, 11, SQ)],
-    [LS(TransPink, 1, TU), LS(TransBlue, 2, TU), LS(Brown, 2, TU), LS(Black, 2, TU), LS(Purple, 13, SQ)],
+    [LS(TransPink, 3, TD), LS(TransBlue, 4, TD), LS(Brown, 4, TD), LS(Black, 4, TD), LS(Red, 21, SQ)],
+    [LS(White, 2, TD), LS(TransPink, 4, TD), LS(TransBlue, 4, TD), LS(Brown, 4, TD), LS(Black, 4, TD), LS(Orange, 18, SQ)],
+    [LS(White, 5, TD), LS(TransPink, 4, TD), LS(TransBlue, 4, TD), LS(Brown, 4, TD), LS(Black, 4, TD), LS(Yellow, 15, SQ)],
+    [LS(White, 5, TU), LS(TransPink, 4, TU), LS(TransBlue, 4, TU), LS(Brown, 4, TU), LS(Black, 4, TU), LS(Green, 15, SQ)],
+    [LS(White, 2, TU), LS(TransPink, 4, TU), LS(TransBlue, 4, TU), LS(Brown, 4, TU), LS(Black, 4, TU), LS(Blue, 18, SQ)],
+    [LS(TransPink, 3, TU), LS(TransBlue, 4, TU), LS(Brown, 4, TU), LS(Black, 4, TU), LS(Purple, 21, SQ)],
 ]
 
 AllyFlag: Flag = [
-    [LS(Black, 10, TU), LS(Red,     2, TD), LS(Black, 8, SQ)],
-    [LS(White,  8, TU), LS(Orange,  6, TD), LS(White, 6, SQ)],
-    [LS(Black,  7, TU), LS(Yellow,  8, TD), LS(Black, 5, SQ)],
-    [LS(White,  5, TU), LS(Green,  12, TD), LS(White, 3, SQ)],
-    [LS(Black,  4, TU), LS(Blue,   6, TU), LS(Black, 2, TD), LS(Blue,   6, TD), LS(Black, 2, SQ)],
-    [LS(White,  2, TU), LS(Purple, 6, TU), LS(White, 6, TD), LS(Purple, 6, TD), LS(White, 0, SQ)],
+    [LS(Black, 18, TU), LS(Red,     3, TD), LS(Black, 15, SQ)],
+    [LS(White, 15, TU), LS(Orange,  9, TD), LS(White, 12, SQ)],
+    [LS(Black, 12, TU), LS(Yellow, 15, TD), LS(Black,  9, SQ)],
+    [LS(White,  9, TU), LS(Green,  21, TD), LS(White,  6, SQ)],
+    [LS(Black,  6, TU), LS(Blue,   12, TU), LS(Black,  3, TD), LS(Blue,   12, TD), LS(Black, 3, SQ)],
+    [LS(White,  3, TU), LS(Purple, 12, TU), LS(White,  9, TD), LS(Purple, 12, TD), LS(White, 0, SQ)],
 ]
 
 TransFlag: Flag = [
-    [LS(TransBlue, 20, SQ)],
-    [LS(TransPink, 20, SQ)],
-    [LS(White,     20, SQ)],
-    [LS(TransPink, 20, SQ)],
-    [LS(TransBlue, 20, SQ)],
+    [LS(TransBlue, 30, SQ)],
+    [LS(TransPink, 30, SQ)],
+    [LS(White,     30, SQ)],
+    [LS(TransPink, 30, SQ)],
+    [LS(TransBlue, 30, SQ)],
+]
+
+FlagOfAustria: Flag = [
+    [LS(Red, 30, SQ)],
+    [LS(Red, 30, SQ)],
+    [LS(White, 30, SQ)],
+    [LS(White, 30, SQ)],
+    [LS(Red, 30, SQ)],
+    [LS(Red, 30, SQ)],
+]
+
+SARed    = (224,  60,  50)
+SAGreen  = (  0, 119,  73)
+SAYellow = (254, 184,  28)
+SABlue   = (  0,  19, 137)
+
+FLagOfSouthAfrica: Flag = [
+    [LS(SAYellow, 1, TD), LS(SAGreen,  7, TD), LS(White,    4, TD), LS(SARed,   28, SQ)],
+    [LS(SAYellow, 4, TD), LS(SAGreen,  7, TD), LS(White,    4, TD), LS(SARed,   25, SQ)],
+    [LS(Black,    3, TD), LS(SAYellow, 4, TD), LS(SAGreen,  7, TD), LS(White,   26, SQ)],
+    [LS(Black,    6, TD), LS(SAYellow, 4, TD), LS(SAGreen, 30, SQ)],
+    [LS(Black,    6, TU), LS(SAYellow, 4, TU), LS(SAGreen, 30, SQ)],
+    [LS(Black,    3, TU), LS(SAYellow, 4, TU), LS(SAGreen,  7, TU), LS(White,   26, SQ)],
+    [LS(SAYellow, 4, TU), LS(SAGreen,  7, TU), LS(White,    4, TU), LS(SABlue,  25, SQ)],
+    [LS(SAYellow, 1, TU), LS(SAGreen,  7, TU), LS(White,    4, TU), LS(SABlue,  28, SQ)],
 ]
 
 def scale_flag(flag: Flag, scale: int) -> Flag:
@@ -113,91 +138,126 @@ def scale_flag(flag: Flag, scale: int) -> Flag:
     return new_flag
 
 def draw_flag(buf: list[str], flag: Flag) -> None:
-    is_odd_line = True
     bg_col: Optional[Color] = None
     fg_col: Optional[Color] = None
 
-    def set_cols(new_bg: Color, new_fg: Color) -> None:
+    def set_cols(new_bg: Optional[Color], new_fg: Optional[Color]) -> None:
         nonlocal buf, bg_col, fg_col
         if bg_col != new_bg:
-            buf.append(f'\x1B[48;2;{new_bg[0]};{new_bg[1]};{new_bg[2]}m')
+            if new_bg is None:
+                buf.append(f'\x1B[49m')
+            else:
+                buf.append(f'\x1B[48;2;{new_bg[0]};{new_bg[1]};{new_bg[2]}m')
             bg_col = new_bg
 
         if fg_col != new_fg:
-            buf.append(f'\x1B[38;2;{new_fg[0]};{new_fg[1]};{new_fg[2]}m')
+            if new_fg is None:
+                buf.append(f'\x1B[38;2;0;0;0m')
+            else:
+                buf.append(f'\x1B[38;2;{new_fg[0]};{new_fg[1]};{new_fg[2]}m')
             fg_col = new_fg
 
-    def set_bg(new_bg: Color) -> None:
+    def set_bg(new_bg: Optional[Color]) -> None:
         nonlocal bg_col, buf
         if bg_col != new_bg:
-            buf.append(f'\x1B[48;2;{new_bg[0]};{new_bg[1]};{new_bg[2]}m')
+            if new_bg is None:
+                buf.append(f'\x1B[49m')
+            else:
+                buf.append(f'\x1B[48;2;{new_bg[0]};{new_bg[1]};{new_bg[2]}m')
             bg_col = new_bg
 
     x = 0
+    max_x = 0
+    prev_carry_x = 0
     is_first = True
     for flag_line in flag:
         if x > 0:
-            if x > 1:
-                buf.append(f'\x1B[{x}D')
+            move = (x + 1) // 2
+            if move > 1:
+                buf.append(f'\x1B[{move}D')
             else:
                 buf.append('\x1B[D')
 
             if not is_first:
                 buf.append('\x1B[B')
+
+            if max_x < x:
+                max_x = x
+
             x = 0
+            prev_carry_x = 0
         is_first = False
 
         for segment_index, segment in enumerate(flag_line):
             col = segment.color
             cap = segment.cap
             length = segment.length
-            x += length
 
-            cap_var1 = is_odd_line
+            if prev_carry_x:
+                length -= 1
+
+            x += length
+            carry_x = x & 1
+            x += carry_x
+
+            next_index = segment_index + 1
+            next_col: Optional[Color]
+            if next_index < len(flag_line):
+                next_col = flag_line[next_index].color
+            else:
+                next_col = None
 
             if cap != LineCap.Square:
-                length -= 2
+                block_length = max(length - 3, 0)
 
-                if length > 0:
+                if block_length > 0:
                     set_bg(col)
-                    buf.append(' ' * length)
+                    buf.append(' ' * (block_length >> 1))
 
-                next_index = segment_index + 1
-                if next_index < len(flag_line):
-                    next_col = flag_line[next_index].color
-                else:
-                    next_col = Black
-
-                if length > -2:
+                cap_length = length - block_length
+                if cap_length > 0:
                     if cap == LineCap.TriDown:
-                        if cap_var1:
-                            set_cols(col, next_col)
-                            if length > -1:
-                                buf.append('🭢')
-                            buf.append('🭕')
-                        else:
+                        if carry_x:
                             set_cols(next_col, col)
-                            if length > -1:
+                            if cap_length > 1:
                                 buf.append('🭏')
                             buf.append('🬼')
+                        else:
+                            set_cols(col, next_col)
+                            if cap_length > 2:
+                                buf.append('🭢')
+                            buf.append('🭕')
+
                     elif cap == LineCap.TriUp:
-                        if cap_var1:
+                        if carry_x:
                             set_cols(next_col, col)
-                            if length > -1:
+                            if cap_length > 1:
                                 buf.append('🭠')
                             buf.append('🭗')
                         else:
                             set_cols(col, next_col)
-                            if length > -1:
+                            if cap_length > 2:
                                 buf.append('🭇')
                             buf.append('🭄')
 
             else:
                 if length > 0:
                     set_bg(col)
-                    buf.append(' ' * length)
+                    buf.append(' ' * (length >> 1))
 
-        is_odd_line = not is_odd_line
+                    if carry_x:
+                        set_cols(next_col, col)
+                        buf.append('▌')
+
+            prev_carry_x = carry_x
+
+    diff = max_x - x
+    move = diff // 2
+    if move > 1:
+        buf.append(f'\x1B[{move}C')
+    elif move > 0:
+        buf.append('\x1B[C')
+
     buf.append('\x1B[0m')
 
 def get_flag_width(flag: Flag) -> int:
@@ -242,6 +302,7 @@ def draw_wrapped_flag_list(buf: list[str], flags: list[Flag], scale: int = 1) ->
     current_width = 0
     for flag in flags:
         width = get_flag_width(flag)
+        width = (width + 1) // 2
         next_width = current_width + width
         if current_width:
             next_width += 2
@@ -263,7 +324,11 @@ def draw_wrapped_flag_list(buf: list[str], flags: list[Flag], scale: int = 1) ->
 def main() -> None:
     buf: list[str] = []
     #print('                               🭇🭒')
-    draw_wrapped_flag_list(buf, [ProgressPrideFlag, AllyFlag, TransFlag], 3)
+    #newlines = len(ProgressPrideFlag) - 1
+    #buf.append('\n' * newlines)
+    #buf.append(f'\x1B[{newlines}A')
+    #draw_flag(buf, ProgressPrideFlag)
+    draw_wrapped_flag_list(buf, [ProgressPrideFlag, AllyFlag, TransFlag, FlagOfAustria, FLagOfSouthAfrica], 1)
     print(''.join(buf))
 
 if __name__ == '__main__':
